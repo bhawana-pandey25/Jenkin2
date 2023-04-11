044a918cf4cdea2cd1219a58e05b84b332b701b3
bhawana pandey <bhawana.pandey@tothenew.com>
2023-04-11 16:31:22 +0530

    commit 2

--- src/test/java/org/ttn/SeleniumMaven/NewTestHome.java
+++ src/test/java/org/ttn/SeleniumMaven/NewTestHome.java
@@ -11,10 +11,9 @@ public class NewTestHome {
  WebDriver driver;
  @Test
  public void checklogo() throws InterruptedException {
-    System.setProperty("webdriver.chrome.driver", "C:\\Users\\Bhawana Pandey\\Downloads\\chromedriver_win32\\"+"chromedriver.exe");
-driver = new ChromeDriver();
-driver.get("https://www.amazon.in/");
-Thread.sleep(2000);
+
+
+	 
 WebElement logo= driver.findElement(By.xpath("//a[@id='nav-logo-sprites']"));
 Assert.assertTrue(logo.isDisplayed());
 Thread.sleep(3000);
